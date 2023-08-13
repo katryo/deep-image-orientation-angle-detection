7474f9149bdd1b23ee102b88acbeae778f16f61e
katryo <katoryo55@gmail.com>
2023-08-13 20:33:53 +0900

    Rotate by 90 degrees

--- infer.py
+++ infer.py
@@ -1,41 +1,58 @@
 import os
+from tqdm import tqdm
 from PIL import Image
 from models import load_vit_model
-import numpy as np
-from processing import preprocess, postprocess
+from processing import preprocess
 from loguru import logger
 import argparse
 
-os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 
 class Inference:
     def __init__(self, load_model_name=None):
         logger.info("Loading Models")
         self.vit_model = load_vit_model()
 
-    
-    def predict(self, model_name, image_path, postprocess_and_save=True):
-        X = preprocess(model_name, image_path)
+    def predict(self, image_path: str, output_dir: str):
+        X = preprocess("vit", image_path)
         
         y = self.vit_model.predict(X)[0][0]
         
         logger.info(f"Predicted angle is: {y} degree")
         pred_angle = -y
-        if postprocess_and_save:
-            postprocess(image_path, pred_angle, 400)
-        return y
 
-        
-        
+        image = Image.open(image_path).convert("RGB")
+
+        image_rotated = image
+        if -135 <= pred_angle < -45:
+            image_rotated = image.rotate(-90, expand=True)
+        elif 45 <= pred_angle < 135:
+            image_rotated = image.rotate(90, expand=True)
+        elif 135 <= pred_angle < 225:
+            image_rotated = image.rotate(180, expand=True)
+
+        basename = os.path.basename(image_path)
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_path = os.path.join(output_dir, basename)
+        image_rotated.save(output_path)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model-name", type=str, default="vit")
-    parser.add_argument("--image-path", type=str, required=True)
+    parser.add_argument("--image-dir", type=str, required=True)
+    parser.add_argument("--output-dir", type=str, required=True)
     args = parser.parse_args()
 
-    model = Inference(load_model_name=args.model_name)
-    model.predict(args.model_name, args.image_path)
-    
-    
+    model = Inference(load_model_name="vit")
+    image_dir = args.image_dir
+    filenames = []
+    for filename in os.listdir(image_dir):
+        if filename.endswith(".png"):
+            filenames.append(filename)
+
+    for filename in tqdm(filenames):
+        image_path = os.path.join(image_dir, filename)
+        model.predict(image_path=image_path, output_dir=args.output_dir)
